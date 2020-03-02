e61fb5a7e18f302ef41c6e9d748b7397d0290435
gavin <justin.project@yahoo.com>
2020-03-02 13:06:56 +0800

    frontend: add signout logic

--- front-end/src/components/common/AppLayout.jsx
+++ front-end/src/components/common/AppLayout.jsx
@@ -1,78 +1,82 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actionCreators/index";
+import { useHistory } from "react-router-dom";
+
 import { Layout, Menu, Icon, Tooltip, Dropdown } from "antd";
 import styles from "./AppLayout.module.scss";
 
 const { Header, Sider, Content } = Layout;
 
-const menu = (
+const menu = (signOut, history) => (
   <Menu>
     <Menu.Item>
       <a
+        href="https://github.com/cnscorpions/React-Nest-Admin"
         target="_blank"
         rel="noopener noreferrer"
-        href="http://www.alipay.com/"
       >
         源码
       </a>
     </Menu.Item>
     <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-        退出
-      </a>
+      <span onClick={() => signOut(history)}>退出</span>
     </Menu.Item>
   </Menu>
 );
 
-class AppLayout extends Component {
-  render() {
-    const { isCollapsed, toggle } = this.props;
+const AppLayout = props => {
+  let history = useHistory();
 
-    return (
-      <Layout className={styles.wrapper}>
-        <Sider trigger={null} collapsible collapsed={isCollapsed}>
-          {this.props.sidebar}
-        </Sider>
-        <Layout>
-          <Header className={styles.header}>
-            <Icon
-              className={styles["trigger"]}
-              type={isCollapsed ? "menu-unfold" : "menu-fold"}
-              onClick={() => toggle(isCollapsed)}
-            />
-            <span className={styles.home}>首页</span>
-            <div className={styles["icon-group"]}>
-              <span className={styles["icon-box"]}>
-                <Icon className={styles.icon} type="fullscreen" />
-              </span>
-              <span className={styles["icon-box"]}>
-                <Tooltip placement="bottom" title="字体大小">
-                  <Icon className={styles.icon} type="font-size" />
-                </Tooltip>
-              </span>
-              <span className={styles["icon-box"]}>
-                <Dropdown overlay={menu} placement="bottomCenter">
-                  <Icon className={styles.icon} type="more" />
-                </Dropdown>
-              </span>
-            </div>
-          </Header>
-          <Content
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              background: "#fff",
-              minHeight: 280
-            }}
-          >
-            {this.props.content}
-          </Content>
-        </Layout>
+  // 结构赋值
+  const { isCollapsed, toggle, signOut } = props;
+
+  return (
+    <Layout className={styles.wrapper}>
+      <Sider trigger={null} collapsible collapsed={isCollapsed}>
+        {props.sidebar}
+      </Sider>
+      <Layout>
+        <Header className={styles.header}>
+          <Icon
+            className={styles["trigger"]}
+            type={isCollapsed ? "menu-unfold" : "menu-fold"}
+            onClick={() => toggle(isCollapsed)}
+          />
+          <span className={styles.home}>首页</span>
+          <div className={styles["icon-group"]}>
+            <span className={styles["icon-box"]}>
+              <Icon className={styles.icon} type="fullscreen" />
+            </span>
+            <span className={styles["icon-box"]}>
+              <Tooltip placement="bottom" title="字体大小">
+                <Icon className={styles.icon} type="font-size" />
+              </Tooltip>
+            </span>
+            <span className={styles["icon-box"]}>
+              <Dropdown
+                overlay={() => menu(signOut, history)}
+                placement="bottomCenter"
+              >
+                <Icon className={styles.icon} type="more" />
+              </Dropdown>
+            </span>
+          </div>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            background: "#fff",
+            minHeight: 280
+          }}
+        >
+          {props.content}
+        </Content>
       </Layout>
-    );
-  }
-}
+    </Layout>
+  );
+};
 
 const mapStateToProps = state => ({
   isCollapsed: state.layout.isCollapsed
@@ -84,8 +88,12 @@ const mapDispatchToProps = dispatch => {
       const action = isCollapsed
         ? actionCreators.spreadSidebar()
         : actionCreators.collapseSidebar();
-      console.log(action);
       dispatch(action);
+    },
+    signOut(history) {
+      const action = actionCreators.unauthenticate();
+      dispatch(action);
+      history.push("/login");
     }
   };
 };
