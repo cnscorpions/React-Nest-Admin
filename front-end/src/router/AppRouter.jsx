ddc6fac5bf953268d7c5fd43fd49942a964ec653
gavin <justin.project@yahoo.com>
2020-03-04 11:56:41 +0800

    frontend: rename auth.js to route-guard.js

--- front-end/src/router/AppRouter.jsx
+++ front-end/src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
-import PrivateRoute from "./auth";
+import PrivateRoute from "./route-guard";
 import { Menu, Icon } from "antd";
 import AppLayout from "../components/common/AppLayout";
 
