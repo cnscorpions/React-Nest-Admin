b8c000690390d49760c776c9bf3d371ba1aa84aa
gavin <justin.project@yahoo.com>
2020-03-09 17:23:51 +0800

    frontend: alter port 13000 to 23000, and run a prod build

---
+++ front-end/build/precache-manifest.d9d670c9199bad2f0911d3b73c20229c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b033f448fb8f19e4198c946c69ab7dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c9752abe84202abcc12",
+    "url": "/static/css/2.c544b106.chunk.css"
+  },
+  {
+    "revision": "38e965659c87d1467802",
+    "url": "/static/css/main.5ba1573f.chunk.css"
+  },
+  {
+    "revision": "3c9752abe84202abcc12",
+    "url": "/static/js/2.a9613079.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.a9613079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e965659c87d1467802",
+    "url": "/static/js/main.5e633562.chunk.js"
+  },
+  {
+    "revision": "691443989d219c585434",
+    "url": "/static/js/runtime-main.1eb18e34.js"
+  }
+]);
--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -3,14 +3,14 @@ import service from "../utils/request";
 
 // ajax req for login to get token
 const checkAuth = (username, password) => {
-  return axios.post("http://localhost:13000/login", {
+  return axios.post("http://localhost:23000/login", {
     username: username,
     password: password
   });
 };
 
 const getProfile = () => {
-  return service.get("http://localhost:13000/profile");
+  return service.get("http://localhost:23000/profile");
 };
 
 export { checkAuth, getProfile };
