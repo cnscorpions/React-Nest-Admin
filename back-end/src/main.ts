95a0721fdcbb1442261ae66d1f5a2244ed07df0c
gavin <justin.project@yahoo.com>
2020-03-09 17:24:52 +0800

    backend: change port from 13000 to 23000

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(13000);
+  await app.listen(23000);
 }
 bootstrap();
